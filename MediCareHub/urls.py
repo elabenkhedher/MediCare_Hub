7ca6ed9e4e799f5ab036378b072679d77f6f6c14
Obayd <oubeidallahzmander@gmail.com>
2026-01-05 22:25:10 +0100

    frdsgf

--- MediCareHub/urls.py
+++ MediCareHub/urls.py
@@ -6,9 +6,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('patients.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth/signin/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('consultations.urls')),
 
 
